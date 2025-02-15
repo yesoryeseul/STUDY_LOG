f3c28f0e33273f9174ba0f6a45550964324a8a19
yesoryeseul <yesoryeseul07@gmail.com>
2025-02-15 15:33:03 +0900

    Builder Pattern 1

---
+++ design_pattern/src/GrimpanMenuBtn.ts
@@ -0,0 +1,200 @@
+// Builder Pattern
+class GrimpanMenuBtn {
+  private name: string;
+  private type: string;
+  private onClick?: () => void;
+  private onChange?: () => void;
+  private active?: boolean;
+  private value?: string | number;
+
+  private constructor(
+    name: string,
+    type: string,
+    onClick?: () => void,
+    onChange?: () => void,
+    active?: boolean,
+    value?: string | number
+  ) {
+    this.name = name;
+    this.type = type;
+    this.onClick = onClick;
+    this.onChange = onChange;
+    this.active = active;
+    this.value = value;
+  }
+
+  static Builder = class GrimpanMenuBuilder {
+    btn: GrimpanMenuBtn;
+    constructor(name: string, type: string) {
+      this.btn = new GrimpanMenuBtn(name, type);
+    }
+    setName(name: string) {
+      this.btn.name = name;
+      return this;
+    }
+    setType(type: string) {
+      this.btn.type = type;
+      return this;
+    }
+
+    setOnClick(onClick: () => void) {
+      this.btn.onClick = onClick;
+      return this;
+    }
+
+    setOnChange(onChange: () => void) {
+      this.btn.onChange = onChange;
+      return this;
+    }
+
+    setActive(active: boolean) {
+      this.btn.active = active;
+      return this;
+    }
+
+    setValue(value: string | number) {
+      this.btn.value = value;
+      return this;
+    }
+
+    build() {
+      return this.btn;
+    }
+  };
+}
+
+// 빌더 교체 방식
+// class GrimpanMenuBtn {
+//   name?: string;
+//   type?: string;
+//   onClick?: () => void;
+//   onChange?: () => void;
+//   active?: boolean;
+//   value?: string | number;
+
+//   constructor(
+//     name?: string,
+//     type?: string,
+//     onClick?: () => void,
+//     onChange?: () => void,
+//     active?: boolean,
+//     value?: string | number
+//   ) {
+//     this.name = name;
+//     this.type = type;
+//     this.onClick = onClick;
+//     this.onChange = onChange;
+//     this.active = active;
+//     this.value = value;
+//   }
+// }
+
+// interface GrimpanMenuBuilder {
+//   setName(name: string): this;
+//   setType(type: string): this;
+//   setOnClick(onClick: () => void): this;
+//   setOnChange(onChange: () => void): this;
+//   setActive(active: boolean): this;
+//   setValue(value: string | number): this;
+//   build(): GrimpanMenuBtn;
+// }
+
+// class ChromGrimpanMenuBtnBuilder implements GrimpanMenuBuilder {
+//   btn: GrimpanMenuBtn;
+//   constructor() {
+//     this.btn = new GrimpanMenuBtn();
+//   }
+
+//   setName(name: string) {
+//     this.btn.name = name;
+//     return this;
+//   }
+//   setType(type: string) {
+//     this.btn.type = type;
+//     return this;
+//   }
+
+//   setOnClick(onClick: () => void) {
+//     this.btn.onClick = onClick;
+//     return this;
+//   }
+
+//   setOnChange(onChange: () => void) {
+//     this.btn.onChange = onChange;
+//     return this;
+//   }
+
+//   setActive(active: boolean) {
+//     this.btn.active = active;
+//     return this;
+//   }
+
+//   setValue(value: string | number) {
+//     this.btn.value = value;
+//     return this;
+//   }
+
+//   build() {
+//     return this.btn;
+//   }
+// }
+// class IEGrimpanMenuBtnBuilder implements GrimpanMenuBuilder {
+//   btn: GrimpanMenuBtn;
+//   constructor() {
+//     this.btn = new GrimpanMenuBtn();
+//   }
+
+//   setName(name: string) {
+//     this.btn.name = name;
+//     return this;
+//   }
+//   setType(type: string) {
+//     this.btn.type = type;
+//     return this;
+//   }
+//   setOnClick(onClick: () => void) {
+//     this.btn.onClick = onClick;
+//     return this;
+//   }
+
+//   setOnChange(onChange: () => void) {
+//     this.btn.onChange = onChange;
+//     return this;
+//   }
+
+//   setActive(active: boolean) {
+//     this.btn.active = active;
+//     return this;
+//   }
+
+//   setValue(value: string | number) {
+//     this.btn.value = value;
+//     return this;
+//   }
+
+//   build() {
+//     return this.btn;
+//   }
+// }
+
+// export class GrimpanMenuBtnDirector {
+//   static createBackBtn(builder: GrimpanMenuBuilder) {
+//     const backBtnBuilder = builder
+//       .setName("뒤로")
+//       .setType("back")
+//       .setOnClick(() => {})
+//       .setActive(false);
+//     return backBtnBuilder;
+//   }
+//   static createForwardBtn(builder: GrimpanMenuBuilder) {
+//     const forwardBtnBuilder = builder
+//       .setName("앞으로")
+//       .setType("forward")
+//       .setOnClick(() => {})
+//       .setActive(false);
+//     return forwardBtnBuilder;
+//   }
+// }
+
+// GrimpanMenuBtnDirector.createBackBtn(new ChromGrimpanMenuBtnBuilder());
+// GrimpanMenuBtnDirector.createForwardBtn(new IEGrimpanMenuBtnBuilder());
